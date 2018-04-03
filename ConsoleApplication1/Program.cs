423db4509ae980f33b009860045d3ace1d6e7a53
wheeliechamp <wheeliechamp2005@yahoo.co.jp>
2018-04-03 18:37:52 +0900

    初回コミット

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      byte[] pdrId = BitConverter.GetBytes((UInt32)1710003000);  //1710003000
+      Console.WriteLine(pdrId[0].ToString());
+      Console.WriteLine(pdrId[0].ToString("X2"));
+      Console.ReadKey();
+    }
+  }
+}
